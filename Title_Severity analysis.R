fe0b5b755943a28d6a96758a86e311a7128d13ae
Sendy Halim <sendyhalim93@gmail.com>
2018-07-02 23:49:21 +0700

    Finishing scraping on hackerone

--- Title_Severity analysis.R
+++ Title_Severity analysis.R
@@ -1,11 +1,11 @@
 ---
-title: "Insight: Node Security Issues"
+  title: "Insight: Node Security Issues"
 author: "Ronauli Silva"
 date: "06/29/2018"
 output: html_notebook
 ---
-
-```{r setup, include=FALSE}
+  
+  ```{r setup, include=FALSE}
 knitr::opts_chunk$set(echo = TRUE)
 library(dplyr) #funny library name
 library(ggplot2)
@@ -40,19 +40,19 @@ So the idea is to see what's happening out there, see common issues, and it woul
 ### Data Source
 
 1. Go to nodesecurity.io/advisories, we get:
-    + Package name
-    + Severity, level of damage. On range 1-10
-    + Version, both vulnerable and patched one(if any)
-    + Link to npm page of respective package
++ Package name
++ Severity, level of damage. On range 1-10
++ Version, both vulnerable and patched one(if any)
++ Link to npm page of respective package
 2. From NSP detail page, we get the hackerOne link.
 3. From hackerone link, we gather these information as strings
-    + Vulnerability, e.g: Interospection graphql query leaks sensitive data.
-    + Impact, e.g: Execute bash script via request parameter
++ Vulnerability, e.g: Interospection graphql query leaks sensitive data.
++ Impact, e.g: Execute bash script via request parameter
 4. From npm page, we gather:
-    + Keywords (easier to handle)
++ Keywords (easier to handle)
 5. From Github API, we get:
-    + Description/functionality of respective package
-    
++ Description/functionality of respective package
+
 ### How
 
 So we run the scraper on node.js, and manage to find the insight using R.
@@ -73,18 +73,18 @@ basic.data <- data.frame(rawData$disclosed_at)
 ```{r}
 titleCorpus <- VCorpus(VectorSource(rawData$title))
 clean <- function(corpus){
-  corpus <- tm_map(corpus, stripWhitespace)
-  corpus <- tm_map(corpus, removePunctuation)
-  corpus <- tm_map(corpus, content_transformer(tolower))
-  corpus <- tm_map(corpus, removeWords, stopwords("en"))
-    return(corpus)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, removeWords, stopwords("en"))
+return(corpus)
 }
 
 cleanFunctionalityData <- clean(titleCorpus)
 # stemming
 
 stemFunc <- content_transformer(function(x){
-  paste(sapply(words(x),stemDocument),collapse = " ")
+paste(sapply(words(x),stemDocument),collapse = " ")
 })
 
 # stemmed functionality string
@@ -105,21 +105,21 @@ d <- data.frame(word = names(sortedData), freq=sortedData)
 
 ## Find a range of y's that'll leave sufficient space above the tallest bar
 bpFreqWord <- barplot(sortedData[1:20]
-        ,main='Most Frequent Word in Titles'
-        ,horiz=TRUE
-        ,xlab="Occurences"
-        ,ylab="Word"
-        ,col=rev(brewer.pal(9, "YlOrRd"))
-        ,las=2
-        ,cex.names=0.8)
+,main='Most Frequent Word in Titles'
+,horiz=TRUE
+,xlab="Occurences"
+,ylab="Word"
+,col=rev(brewer.pal(9, "YlOrRd"))
+,las=2
+,cex.names=0.8)
 
 ggplot(data =yy  , aes(x = word, y = freq, fill=-freq)) +
-    geom_bar(stat="identity") +
-    coord_flip() +
-    scale_color_gradient2(low = "lightblue", high = "darkblue", midpoint=75) +
-    labs(title = 'Word Occurences on Reports`s Title',
-         x = 'Words',
-         y = 'Occurences') 
+geom_bar(stat="identity") +
+coord_flip() +
+scale_color_gradient2(low = "lightblue", high = "darkblue", midpoint=75) +
+labs(title = 'Word Occurences on Reports`s Title',
+x = 'Words',
+y = 'Occurences') 
 ```
 
 #### Title - Severity Relationships 
@@ -133,15 +133,15 @@ df <- df[!(is.na(df$title) | df$title==""), ]
 
 # create a document term matrix 
 dtm <- CreateDtm(doc_vec = df$title, # character vector of documents
-                 doc_names = df$id, # document names
-                 ngram_window = c(1, 2), # minimum and maximum n-gram length
-                 stopword_vec = c(tm::stopwords("english"), # stopwords from tm
-                                  tm::stopwords("SMART")), # this is the default value
-                 lower = TRUE, # lowercase - this is the default value
-                 remove_punctuation = TRUE, # punctuation - this is the default
-                 remove_numbers = TRUE, # numbers - this is the default
-                 verbose = FALSE, # Turn off status bar for this demo
-                 cpus = 2) # default is all available cpus on the system
+doc_names = df$id, # document names
+ngram_window = c(1, 2), # minimum and maximum n-gram length
+stopword_vec = c(tm::stopwords("english"), # stopwords from tm
+tm::stopwords("SMART")), # this is the default value
+lower = TRUE, # lowercase - this is the default value
+remove_punctuation = TRUE, # punctuation - this is the default
+remove_numbers = TRUE, # numbers - this is the default
+verbose = FALSE, # Turn off status bar for this demo
+cpus = 2) # default is all available cpus on the system
 
 # construct the matrix of term counts to get the IDF vector
 tf_mat <- TermDocFreq(dtm)
@@ -160,29 +160,29 @@ hc <- hclust(cdist, method="ward.D")
 clustering <- cutree(hc, 10)
 
 plot(hc, main = "Hierarchical clustering of 100 NIH grant abstracts",
-     ylab = "", xlab = "", yaxt = "n")
+ylab = "", xlab = "", yaxt = "n")
 
 # rect.hclust(hc, 10, border = "red") #uncomment to see the hierarchical clustering
 
 p_words <- colSums(dtm) / sum(dtm)
 
 cluster_words <- lapply(unique(clustering), function(x){
-  rows <- dtm[ clustering == x , ]
-  
-  # for memory's sake, drop all words that don't appear in the cluster
-  rows <- rows[ , colSums(rows) > 0 ]
-  
-  colSums(rows) / sum(rows) - p_words[ colnames(rows) ]
+rows <- dtm[ clustering == x , ]
+
+# for memory's sake, drop all words that don't appear in the cluster
+rows <- rows[ , colSums(rows) > 0 ]
+
+colSums(rows) / sum(rows) - p_words[ colnames(rows) ]
 })
 # create a summary table of the top 5 words defining each cluster
 cluster_summary <- data.frame(cluster = unique(clustering),
-                              size = as.numeric(table(clustering)),
-                              top_words = sapply(cluster_words, function(d){
-                                paste(
-                                  names(d)[ order(d, decreasing = TRUE) ][ 1:5 ], 
-                                  collapse = ", ")
-                              }),
-                              stringsAsFactors = FALSE)
+size = as.numeric(table(clustering)),
+top_words = sapply(cluster_words, function(d){
+paste(
+names(d)[ order(d, decreasing = TRUE) ][ 1:5 ], 
+collapse = ", ")
+}),
+stringsAsFactors = FALSE)
 cluster_summary
 ```
 
@@ -190,10 +190,10 @@ Wordcloud of First Cluster (xss)
 ```{r}
 
 wordcloud(words = names(cluster_words[[1]]), 
-                     freq = cluster_words[[1]],
-                     random.order = F,
-                     colors=brewer.pal(8, "Set2"),
-                     main = "Top words in cluster 100")
+freq = cluster_words[[1]],
+random.order = F,
+colors=brewer.pal(8, "Set2"),
+main = "Top words in cluster 100")
 
 ```
 
